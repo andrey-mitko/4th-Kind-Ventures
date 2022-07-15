59269e6c07c23746035dcddb95583ad51308e3e7
Andrey Mitko <andrey@crowdhaus.co.uk>
2022-07-15 16:19:21 +0100

    Toggling Navigation

--- 4thkind.js
+++ 4thkind.js
@@ -1,10 +1,25 @@
 const mqDark = window.matchMedia("(prefers-color-scheme: dark)")
 const darkModeToggle = document.querySelector("a.dark-mode-toggle")
 const darkModeToggleText = darkModeToggle.querySelector("span")
+const menuToggle = document.querySelector("a.menu-toggle")
+const menuToggleText = menuToggle.querySelector("span")
 
+const bodyTag = document.querySelector("body");
+
+menuToggle.addEventListener("click", () => {
+    bodyTag.classList.toggle("nav-open");
+    menuToggleText.innerHTML = bodyTag.classList.contains("nav-open") ? "Close" : "Menu";
+})
+
+
+darkModeToggle.addEventListener("click", () => {
+    bodyTag.classList.toggle("dark-mode");
+    darkModeToggleText.innerHTML = bodyTag.classList.contains("dark-mode") ? "Light mode" : "Dark mode";
+});
 
 const updateDarkMode = () => {
     darkModeToggleText.innerHTML = mqDark.matches ? "Light mode" : "Dark mode"
+    mqDark.matches ? bodyTag.classList.add("dark-mode") : bodyTag.classList.remove("dark-mode");
 };
 
 updateDarkMode();   
